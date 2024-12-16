3b5f60b6b16b16c1c61a5b639f6b935c0dd439bd
admin <astannentag@gmail.com>
2024-12-16 19:04:09 +0300

    Коммит 2

--- pythonProject2/main.py
+++ pythonProject2/main.py
@@ -1,40 +1,58 @@
-from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QRect
 import sys
 import random
+from PyQt6.QtWidgets import QApplication
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setWindowTitle("Случайные окружности")
+        self.setGeometry(100, 100, 800, 600)
 
-        # Найти кнопку из UI и подключить событие
-        self.drawButton = self.findChild(QPushButton, 'drawButton')
-        self.drawButton.clicked.connect(self.add_circle)
+        # Виджет
+        self.central_widget = QWidget()
+        self.layout = QVBoxLayout(self.central_widget)
+
+        # Кнопка
+        self.draw_button = QPushButton("Нарисовать окружности")
+        self.layout.addWidget(self.draw_button)
+        self.draw_button.setFixedSize(200, 50)
+        self.draw_button.move(700, 500)
+
+        # Основное окно
+        self.setCentralWidget(self.central_widget)
 
         # Список кругов
         self.circles = []
 
+        # Сигнал-слот
+        self.draw_button.clicked.connect(self.add_circle)
+
     def add_circle(self):
-        # Добавляем рандомные круги
+        # Добавляем рандомные круги с цветом
         x = random.randint(50, self.width() - 50)
         y = random.randint(50, self.height() - 50)
         radius = random.randint(10, 100)
-        self.circles.append((x, y, radius))
+        color = QColor(
+            random.randint(0, 255),
+            random.randint(0, 255),
+            random.randint(0, 255)
+        )
+        self.circles.append((x, y, radius, color))
         self.update()  # Перерисовать форму
 
     def paintEvent(self, event):
         # Рисуем круги
         painter = QPainter(self)
-        painter.setBrush(QColor('yellow'))
-        for x, y, radius in self.circles:
+        for x, y, radius, color in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(QRect(x, y, radius, radius))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
